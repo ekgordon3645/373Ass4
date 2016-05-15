fe1a2a4ee53d8b50a3fc343554bc185f4fe82f70
Evan Gordon <Evan Gordon>
2016-05-15 12:35:29 -0700

    Changed formatting

--- src/ChainingHash.java
+++ src/ChainingHash.java
@@ -8,159 +8,159 @@
  */
 
 public class ChainingHash {
-		public static final int DEFAULT_TABLE_SIZE = 12553; // Prime default hashTable size
-        public static final int HASH_CONSTANT = 229; // Prime multiplicative constant for hash function
-		private ChainingNode[] hashTable;
-        private int tableIndex; // Tracks current hashTable position for getNextKey()
-        private int listIndex; // Tracks current key for getNextKey()
-        private int wordCount; // Tracks total word count of all keys in hashTable
-        private int keyCount; // Tracks total number of unique keys in hashTable
-        private int loadCount; // Tracks total number of non-null indices in hashTable for load factor
+    public static final int DEFAULT_TABLE_SIZE = 12553; // Prime default hashTable size
+    public static final int HASH_CONSTANT = 229; // Prime multiplicative constant for hash function
+    private ChainingNode[] hashTable;
+    private int tableIndex; // Tracks current hashTable position for getNextKey()
+    private int listIndex; // Tracks current key for getNextKey()
+    private int wordCount; // Tracks total word count of all keys in hashTable
+    private int keyCount; // Tracks total number of unique keys in hashTable
+    private int loadCount; // Tracks total number of non-null indices in hashTable for load factor
 
-        /**
-         * Instantiates a ChainingHash with the default hash table size.
-         */
-		public ChainingHash(){
-            this(DEFAULT_TABLE_SIZE);
-		}
+    /**
+     * Instantiates a ChainingHash with the default hash table size.
+     */
+    public ChainingHash(){
+        this(DEFAULT_TABLE_SIZE);
+    }
 
-        /**
-        * Instantiates a ChainingHash with the desired hash table size.
-        * @param startSize : Desired size of hash table
-        */
-		public ChainingHash(int startSize){
-			hashTable = new ChainingNode[startSize];
-            wordCount = 0;
-            tableIndex = 0;
-            listIndex = 0;
-            loadCount = 0;
-		}
+    /**
+     * Instantiates a ChainingHash with the desired hash table size.
+     * @param startSize : Desired size of hash table
+     */
+    public ChainingHash(int startSize){
+        hashTable = new ChainingNode[startSize];
+        wordCount = 0;
+        tableIndex = 0;
+        listIndex = 0;
+        loadCount = 0;
+    }
 
-		/**
-		 * This function allows rudimentary iteration through the ChainingHash.
-		 * The ordering is not important so long as all added elements are returned only once.
-		 * It should return null once it has gone through all elements
-		 * @return Returns the next element of the hash table. Returns null if it is at its end.
-		 */
-		public String getNextKey(){
-            String nextKey = null;
-            while (tableIndex < hashTable.length && nextKey == null) {
-                if (hashTable[tableIndex] != null) { // Found an entry; examine its list
-                    ChainingNode current = hashTable[tableIndex];
-                    for (int i = 0; i < listIndex; i++) { // Traverse to current list-position
-                        current = current.next;
-                    }
-                    listIndex++;
-                    if (current.next == null) { // No keys remain for this index of hashTable
-                        listIndex = 0; // Ensure we look at the beginning of next node's list
-                        tableIndex++; // Move to next index of hashTable
-                    }
-                    nextKey = current.hashedString;
-                } else { // Continue searching hashTable for nodes that are not null
-                    tableIndex++;
+    /**
+     * This function allows rudimentary iteration through the ChainingHash.
+     * The ordering is not important so long as all added elements are returned only once.
+     * It should return null once it has gone through all elements
+     * @return Returns the next element of the hash table. Returns null if it is at its end.
+     */
+    public String getNextKey(){
+        String nextKey = null;
+        while (tableIndex < hashTable.length && nextKey == null) {
+            if (hashTable[tableIndex] != null) { // Found an entry; examine its list
+                ChainingNode current = hashTable[tableIndex];
+                for (int i = 0; i < listIndex; i++) { // Traverse to current list-position
+                    current = current.next;
                 }
-            }
-			return nextKey;
-		}
-
-        /**
-		 * Adds the key to the hash table.
-		 * If there is a collision, it should be dealt with by chaining the keys together.
-		 * If the key is already in the hash table, it increments that key's counter.
-		 * @param keyToAdd : the key which will be added to the hash table
-		 */
-		public void insert(String keyToAdd){
-            wordCount++;
-            int hashCode = hash(keyToAdd);
-            if (hashTable[hashCode] == null) { // We must utilize another index of the hashTable; increment loadCount
-                loadCount++;
-            }
-            ChainingNode current = hashTable[hashCode];
-
-            // Check each node in list for the desired key
-            while (current != null && !current.hashedString.equals(keyToAdd)) {
-                current = current.next;
-            }
-            if (current == null) { // Key was not in list; must create new entry for this key.
-                current = new ChainingNode(keyToAdd); // keyToAdd is not in list; create new node
-                current.next = hashTable[hashCode];
-                hashTable[hashCode] = current;
-                keyCount++;
-            }
-            assert current.hashedString.equals(keyToAdd);
-            current.count++;
-		}
-
-        /**
-		 * Returns the number of times a key has been added to the hash table.
-		 * @param keyToFind : The key being searched for
-		 * @return returns the number of times that key has been added.
-		 */
-		public int findCount(String keyToFind){
-            int hashCode = hash(keyToFind);
-            ChainingNode current = hashTable[hashCode];
-            while (current != null) {
-                if (current.hashedString.equals(keyToFind)) {
-                    return current.count;
+                listIndex++;
+                if (current.next == null) { // No keys remain for this index of hashTable
+                    listIndex = 0; // Ensure we look at the beginning of next node's list
+                    tableIndex++; // Move to next index of hashTable
                 }
-                current = current.next;
+                nextKey = current.hashedString;
+            } else { // Continue searching hashTable for nodes that are not null
+                tableIndex++;
             }
-            return 0;
-		}
+        }
+        return nextKey;
+    }
 
-        /**
-         * Generates a hashcode for the desired key
-         * @param keyToHash : The key being hashed; case-sensitive.
-         * @return returns the hashcode generated
-         */
-		private int hash(String keyToHash){
-            int hashCode = 0;
-            for (int i = 0; i < keyToHash.length(); i++) {
-                hashCode = HASH_CONSTANT * hashCode + keyToHash.charAt(i);
+    /**
+     * Adds the key to the hash table.
+     * If there is a collision, it should be dealt with by chaining the keys together.
+     * If the key is already in the hash table, it increments that key's counter.
+     * @param keyToAdd : the key which will be added to the hash table
+     */
+    public void insert(String keyToAdd){
+        wordCount++;
+        int hashCode = hash(keyToAdd);
+        if (hashTable[hashCode] == null) { // We must utilize another index of the hashTable; increment loadCount
+            loadCount++;
+        }
+        ChainingNode current = hashTable[hashCode];
+
+        // Check each node in list for the desired key
+        while (current != null && !current.hashedString.equals(keyToAdd)) {
+            current = current.next;
+        }
+        if (current == null) { // Key was not in list; must create new entry for this key.
+            current = new ChainingNode(keyToAdd); // keyToAdd is not in list; create new node
+            current.next = hashTable[hashCode];
+            hashTable[hashCode] = current;
+            keyCount++;
+        }
+        assert current.hashedString.equals(keyToAdd);
+        current.count++;
+    }
+
+    /**
+     * Returns the number of times a key has been added to the hash table.
+     * @param keyToFind : The key being searched for
+     * @return returns the number of times that key has been added.
+     */
+    public int findCount(String keyToFind){
+        int hashCode = hash(keyToFind);
+        ChainingNode current = hashTable[hashCode];
+        while (current != null) {
+            if (current.hashedString.equals(keyToFind)) {
+                return current.count;
             }
-            hashCode %= hashTable.length;
-            hashCode = Math.abs(hashCode);
-            assert hashCode >= 0 && hashCode <= hashTable.length;
-            return hashCode;
-		}
-
-        /**
-         * Returns the total count of all words in the hash table
-         * @return : Returns the total number of words in the hash table
-         */
-        public int getWordCount() {
-            return wordCount;
+            current = current.next;
         }
+        return 0;
+    }
 
-        /**
-         * Returns the number of keys in the hash table
-         * @return : Returns the total number of keys in the hash table
-         */
-        public int getKeyCount() {
-            return keyCount;
+    /**
+     * Generates a hashcode for the desired key
+     * @param keyToHash : The key being hashed; case-sensitive.
+     * @return returns the hashcode generated
+     */
+    private int hash(String keyToHash){
+        int hashCode = 0;
+        for (int i = 0; i < keyToHash.length(); i++) {
+            hashCode = HASH_CONSTANT * hashCode + keyToHash.charAt(i);
         }
+        hashCode %= hashTable.length;
+        hashCode = Math.abs(hashCode);
+        assert hashCode >= 0 && hashCode <= hashTable.length;
+        return hashCode;
+    }
 
-        /**
-         * Computes the load factor (lambda) of the hashTable
-         * @return : Returns the computed load factor
-         */
-        public double loadFactor() {
-            return (double) loadCount / hashTable.length;
+    /**
+     * Returns the total count of all words in the hash table
+     * @return : Returns the total number of words in the hash table
+     */
+    public int getWordCount() {
+        return wordCount;
+    }
+
+    /**
+     * Returns the number of keys in the hash table
+     * @return : Returns the total number of keys in the hash table
+     */
+    public int getKeyCount() {
+        return keyCount;
+    }
+
+    /**
+     * Computes the load factor (lambda) of the hashTable
+     * @return : Returns the computed load factor
+     */
+    public double loadFactor() {
+        return (double) loadCount / hashTable.length;
+    }
+
+    /**
+     * Simple class that stores a single hashed key value and its count,
+     * as well as a reference to the next node with the same hashCode, i.e.
+     * the next node that stores a key with the same hashCode.
+     */
+    private class ChainingNode {
+        public ChainingNode next;
+        public String hashedString; // Key value hashed to this node
+        public int count; // Count of key value
+
+        public ChainingNode(String toHash) {
+            hashedString = toHash;
+            count = 0;
         }
-
-        /**
-         * Simple class that stores a single hashed key value and its count,
-         * as well as a reference to the next node with the same hashCode, i.e.
-         * the next node that stores a key with the same hashCode.
-         */
-		private class ChainingNode {
-			public ChainingNode next;
-			public String hashedString; // Key value hashed to this node
-			public int count; // Count of key value
-
-            public ChainingNode(String toHash) {
-                hashedString = toHash;
-                count = 0;
-            }
-		}
+    }
 }
--- src/QPHash.java
+++ src/QPHash.java
@@ -8,163 +8,163 @@
  */
 
 public class QPHash {
-		public static final int DEFAULT_TABLE_SIZE = 12553; // Prime default hashTable size
-		public static final int HASH_CONSTANT = 229; // Prime multiplicative constant for hash function
-		private QPNode[] hashTable;
-		private int tableIndex; // Tracks current index for getNextKey()
-		private int wordCount; // Tracks total word count of all keys in hashTable
-		private int keyCount; // Tracks total number of keys stored in hashTable for load factor
+	public static final int DEFAULT_TABLE_SIZE = 12553; // Prime default hashTable size
+	public static final int HASH_CONSTANT = 229; // Prime multiplicative constant for hash function
+	private QPNode[] hashTable;
+	private int tableIndex; // Tracks current index for getNextKey()
+	private int wordCount; // Tracks total word count of all keys in hashTable
+	private int keyCount; // Tracks total number of keys stored in hashTable for load factor
 
-		/**
-		 * Instantiates a QPHash with the default hash table size.
-		 */
-		public QPHash(){
-			this(DEFAULT_TABLE_SIZE);
-		}
+	/**
+	 * Instantiates a QPHash with the default hash table size.
+	 */
+	public QPHash(){
+		this(DEFAULT_TABLE_SIZE);
+	}
 
-		/**
-		 * Instantiates a QPHash with the desired hash table size.
-		 * @param startSize : Desired size of hash table
-		 */
-		public QPHash(int startSize){
-			hashTable = new QPNode[startSize];
-			tableIndex = 0;
-			wordCount = 0;
-			keyCount = 0;
-		}
+	/**
+	 * Instantiates a QPHash with the desired hash table size.
+	 * @param startSize : Desired size of hash table
+	 */
+	public QPHash(int startSize){
+		hashTable = new QPNode[startSize];
+		tableIndex = 0;
+		wordCount = 0;
+		keyCount = 0;
+	}
 
-		/**
-		 * This function allows rudimentary iteration through the QPHash.
-		 * The ordering is not important so long as all added elements are returned only once.
-		 * It should return null once it has gone through all elements
-		 * @return Returns the next element of the hash table. Returns null if it is at its end.
-		 */
-		public String getNextKey(){
-			while (tableIndex < hashTable.length) {
-				if (hashTable[tableIndex] != null) {
-					return hashTable[tableIndex++].hashedString;
-				}
-				tableIndex++;
+	/**
+	 * This function allows rudimentary iteration through the QPHash.
+	 * The ordering is not important so long as all added elements are returned only once.
+	 * It should return null once it has gone through all elements
+	 * @return Returns the next element of the hash table. Returns null if it is at its end.
+	 */
+	public String getNextKey(){
+		while (tableIndex < hashTable.length) {
+			if (hashTable[tableIndex] != null) {
+				return hashTable[tableIndex++].hashedString;
 			}
-			return null;
+			tableIndex++;
 		}
+		return null;
+	}
 
-		/**
-		 * Adds the key to the hash table.
-		 * If there is a collision, a new location should be found using quadratic probing.
-		 * If the key is already in the hash table, it increments that key's counter.
-		 * @param keyToAdd : the key which will be added to the hash table; case-sensitive.
-		 */
-		public void insert(String keyToAdd) {
-			wordCount++;
-			int hashCode = hash(keyToAdd);
-			if (hashTable[hashCode] == null) {
-				hashTable[hashCode] = new QPNode(keyToAdd);
-				keyCount++;
-			}
-			assert hashTable[hashCode].hashedString.equals(keyToAdd);
-			hashTable[hashCode].count++;
+	/**
+	 * Adds the key to the hash table.
+	 * If there is a collision, a new location should be found using quadratic probing.
+	 * If the key is already in the hash table, it increments that key's counter.
+	 * @param keyToAdd : the key which will be added to the hash table; case-sensitive.
+	 */
+	public void insert(String keyToAdd) {
+		wordCount++;
+		int hashCode = hash(keyToAdd);
+		if (hashTable[hashCode] == null) {
+			hashTable[hashCode] = new QPNode(keyToAdd);
+			keyCount++;
 		}
+		assert hashTable[hashCode].hashedString.equals(keyToAdd);
+		hashTable[hashCode].count++;
+	}
 
-		/**
-		 * Returns the number of times a key has been added to the hash table.
-		 * @param keyToFind : The key being searched for; case-sensitive.
-		 * @return returns the number of times that key has been added.
-		 */
-		public int findCount(String keyToFind){
-			int hashCode = hash(keyToFind);
-			if (hashTable[hashCode] != null) {
-				return hashTable[hashCode].count;
-			}
-			return 0;
+	/**
+	 * Returns the number of times a key has been added to the hash table.
+	 * @param keyToFind : The key being searched for; case-sensitive.
+	 * @return returns the number of times that key has been added.
+	 */
+	public int findCount(String keyToFind){
+		int hashCode = hash(keyToFind);
+		if (hashTable[hashCode] != null) {
+			return hashTable[hashCode].count;
 		}
+		return 0;
+	}
 
-		/**
-		 * Generates a hashcode for the desired key
-		 * @param keyToHash : The key being hashed; case-sensitive.
-		 * @return returns the hashcode generated
-		 */
-		private int hash(String keyToHash){
-			int hashCode = 0;
+	/**
+	 * Generates a hashcode for the desired key
+	 * @param keyToHash : The key being hashed; case-sensitive.
+	 * @return returns the hashcode generated
+	 */
+	private int hash(String keyToHash){
+		int hashCode = 0;
 
-			// Generate initial hashCode
-			for (int i = 0; i < keyToHash.length(); i++) {
-				hashCode = HASH_CONSTANT * hashCode + keyToHash.charAt(i);
-			}
-			hashCode %= hashTable.length;
-			hashCode = Math.abs(hashCode);
-			hashCode = probe(hashCode, keyToHash);
-			assert hashCode >= 0 && hashCode <= hashTable.length;
+		// Generate initial hashCode
+		for (int i = 0; i < keyToHash.length(); i++) {
+			hashCode = HASH_CONSTANT * hashCode + keyToHash.charAt(i);
+		}
+		hashCode %= hashTable.length;
+		hashCode = Math.abs(hashCode);
+		hashCode = probe(hashCode, keyToHash);
+		assert hashCode >= 0 && hashCode <= hashTable.length;
+		return hashCode;
+	}
+
+	/**
+	 * Returns the number of words in the hash table
+	 * @return : Returns the total count of all words in the hash table
+	 */
+	public int getWordCount() {
+		return wordCount;
+	}
+
+	/**
+	 * Returns the number of keys in the hash table
+	 * @return : Returns the total number of keys in the hash table
+	 */
+	public int getKeyCount() {
+		return keyCount;
+	}
+
+	/**
+	 * Computes the load factor (lambda) of the hashTable
+	 * @return : Returns the computed load factor
+	 */
+	public double loadFactor() {
+		return (double) keyCount / hashTable.length;
+	}
+
+	/**
+	 * Checks for possible hashCode collisions and corrects for them
+	 * @param hashCode : Hashcode to examine for collisions
+	 * @param keyToHash : Key that hashCode was generated from
+	 * @return Returns same hashCode if no collisions were detected; If there were collisions,
+	 * 			returns a hashCode modified to correct for possible collisions.
+	 */
+	private int probe(int hashCode, String keyToHash) {
+		boolean collisionDetected = hashTable[hashCode] != null &&
+				!hashTable[hashCode].hashedString.equals(keyToHash);
+
+		// Modification of generated hashCode not necessary
+		if (!collisionDetected) {
 			return hashCode;
 		}
 
-		/**
-		 * Returns the number of words in the hash table
-		 * @return : Returns the total count of all words in the hash table
-		 */
-		public int getWordCount() {
-			return wordCount;
-		}
+		// Correct possible collision using quadratic probing
+		int collisionFactor = 1;
+		while (collisionDetected) {
+			hashCode += Math.pow(collisionFactor, 2); // Adjust hashcode by squared collision factor
+			if (hashCode >= hashTable.length) {
+				hashCode -= hashTable.length;
+			}
 
-		/**
-		 * Returns the number of keys in the hash table
-		 * @return : Returns the total number of keys in the hash table
-		 */
-		public int getKeyCount() {
-			return keyCount;
-		}
-
-		/**
-		 * Computes the load factor (lambda) of the hashTable
-		 * @return : Returns the computed load factor
-		 */
-		public double loadFactor() {
-			return (double) keyCount / hashTable.length;
-		}
-
-		/**
-		 * Checks for possible hashCode collisions and corrects for them
-		 * @param hashCode : Hashcode to examine for collisions
-		 * @param keyToHash : Key that hashCode was generated from
-		 * @return Returns same hashCode if no collisions were detected; If there were collisions,
-		 * 			returns a hashCode modified to correct for possible collisions.
-		 */
-		private int probe(int hashCode, String keyToHash) {
-			boolean collisionDetected = hashTable[hashCode] != null &&
+			// Check if there is still a collision
+			collisionDetected = hashTable[hashCode] != null &&
 					!hashTable[hashCode].hashedString.equals(keyToHash);
-
-			// Modification of generated hashCode not necessary
-			if (!collisionDetected) {
-				return hashCode;
-			}
-
-			// Correct possible collision using quadratic probing
-			int collisionFactor = 1;
-			while (collisionDetected) {
-				hashCode += Math.pow(collisionFactor, 2); // Adjust hashcode by squared collision factor
-				if (hashCode >= hashTable.length) {
-					hashCode -= hashTable.length;
-				}
-
-				// Check if there is still a collision
-				collisionDetected = hashTable[hashCode] != null &&
-						!hashTable[hashCode].hashedString.equals(keyToHash);
-				collisionFactor++;
-			}
-			assert hashTable[hashCode] == null || hashTable[hashCode].hashedString.equals(keyToHash);
-			return hashCode;
+			collisionFactor++;
 		}
+		assert hashTable[hashCode] == null || hashTable[hashCode].hashedString.equals(keyToHash);
+		return hashCode;
+	}
 
-		/**
-		 * Simple class that stores a single hashed key value and its count
-		 */
-		private class QPNode {
-			public String hashedString; // Key value hashed to this node
-			public int count; // Count of key
+	/**
+	 * Simple class that stores a single hashed key value and its count
+	 */
+	private class QPNode {
+		public String hashedString; // Key value hashed to this node
+		public int count; // Count of key
 
-			public QPNode(String toHash) {
-				hashedString = toHash;
-				count = 0;
-			}
+		public QPNode(String toHash) {
+			hashedString = toHash;
+			count = 0;
 		}
+	}
 }
